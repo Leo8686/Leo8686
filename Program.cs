7d326dd991e83720b592853ace1caf84ae2d8bdf
Leonardo Mota <107980029+Leo8686@users.noreply.github.com>
2022-06-22 03:21:30 -0300

    Add files via upload
    Programa para cálculo de idade.

--- Program.cs
+++ Program.cs
@@ -1,38 +1,12 @@
-﻿class Conclusao
+﻿static object Main(object? @int)
 {
-    static void Main()
-    {
-        bool opcao = true;
 
-        Console.WriteLine("Olá, irei realizar a soma de números até você parar!");
+    Console.WriteLine("Qual seu nome?");
+    String nome = Console.ReadLine();
+    Console.WriteLine("Quantos anos você tem?");
+    Int idade object Int = null;
+    -int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Digite o valor inicial!");
-        float.TryParse(Console.ReadLine(), out float inicial);
+    Console.WriteLine("Seu nome é {0} e sua idade é de {1}", nome, idade);
 
-        while (opcao == true)
-        {
-
-            Console.WriteLine("Digite o número que deseja somar ao número inicial: ");
-            float.TryParse(Console.ReadLine(), out float n1);
-            inicial = inicial + n1;
-            Console.WriteLine("A soma até o momento é {0}", inicial);
-            Console.WriteLine("Deseja sair do programa? 1 - Sim / 2 - Não");
-            int.TryParse(Console.ReadLine(), out int saida);
-
-            switch (saida)
-            {
-                case 1:
-                    Console.WriteLine("Saindo do Programa!");
-                    opcao = false;
-                    break;
-                case 2:
-                    Console.WriteLine("Continua a Soma...");
-                    break;
-                default:
-                    Console.WriteLine("Opção Inválida.Retornando...");
-                    break;
-            }
-
-        }
-    }
 }
